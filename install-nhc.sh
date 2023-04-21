def97bb70a1c9b83cbdeab9740d057ca612cd8e2
Ubuntu <azureuser@rajvi-nd96amsr-v4.vfmz02qryxyundbl3q24ozwj4g.ax.internal.cloudapp.net>
2023-04-21 21:18:07 +0000

    Call custom test set up script

--- install-nhc.sh
+++ install-nhc.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 NHC_VERSION=1.4.3
+echo "Installed NHC verison $NHC_VERSION"
 
 wget -O nhc-$NHC_VERSION.tar.xz https://github.com/mej/nhc/releases/download/1.4.3/lbnl-nhc-1.4.3.tar.xz
 tar -xf nhc-$NHC_VERSION.tar.xz
 
 cd lbnl-nhc-$NHC_VERSION
 ./configure --prefix=/usr --sysconfdir=/etc --libexecdir=/usr/libexec
+
 sudo make test
+echo -e "\n"
 sudo make install
+
+cd ..
+echo -e "\nRunning set up script for custom tests"
+./customTests/custom-test-setup.sh
