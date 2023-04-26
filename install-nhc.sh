2d68460cb7c766deb5cfae7acf3be7238b578616
Ubuntu <rafsalas@ub22h3e8f000003.dqjt2vfkzmou3mpiz4ibgqqi1c.ax.internal.cloudapp.net>
2023-04-26 19:27:44 +0000

    another fix to install and fix to file permissions

--- install-nhc.sh
+++ install-nhc.sh
@@ -8,11 +8,11 @@ tar -xf nhc-$NHC_VERSION.tar.xz
 
 pushd lbnl-nhc-$NHC_VERSION
 ./configure --prefix=/usr --sysconfdir=/etc --libexecdir=/usr/libexec
-popd
 
 sudo make test
 echo -e "\n"
 sudo make install
+popd
 
 echo -e "\nRunning set up script for custom tests"
 pushd customTests/
