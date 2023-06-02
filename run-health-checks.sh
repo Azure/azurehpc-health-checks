7c8136c6387c950048d7a8b131b7fe3bd6c43466
almalinux Cloud User <azhpcuser@hx17666c200004E.aeid2alhjcluxleorasxbfdxtb.bx.internal.cloudapp.net>
2023-06-02 16:46:22 +0000

    ignore case

--- run-health-checks.sh
+++ run-health-checks.sh
@@ -3,18 +3,20 @@
 SKU=$( curl -H Metadata:true --max-time 10 -s "http://169.254.169.254/metadata/instance/compute/vmSize?api-version=2021-01-01&format=text")
 echo "Running health checks for $SKU SKU..."
 
-if [ $SKU = "Standard_ND96asr_v4" ]; then
+if [ "${SKU,,}" = "standard_nd96asr_v4" ]; then
     conf_name="nd96asr_v4"
-elif [ $SKU = "Standard_ND96amsr_A100_v4" ]; then
+elif [ "${SKU,,}" = "standard_nd96amsr_a100_v4" ]; then
     conf_name="nd96amsr_v4"
-elif [ $SKU = "Standard_ND96isr_H100_v5" ]; then
+elif [ "${SKU,,}" = "standard_nd96isr_h100_v5" ]; then
     conf_name="nd96isr_v5"
-elif [ $SKU = "Standard_HB120rs_v2" ]; then
+elif [ "${SKU,,}" = "standard_hb120rs_v2" ]; then
     conf_name="hb120rs_v2"
-elif [ $SKU = "Standard_HB120rs_v3" ]; then
+elif [ "${SKU,,}" = "standard_hb120rs_v3" ]; then
     conf_name="hb120rs_v3"
-elif [ $SKU = "Standard_HBv4_176" ]; then
+elif [ "${SKU,,}" = "standard_hbv4_176" ]; then
     conf_name="hbv4_176"
+elif [ "${SKU,,}" = "standard_hx176rs" ]; then
+    conf_name="hx176rs_4"
 else
     echo "SKU: $SKU check not currently implemented"
     exit 1
