00f42e3565e7476c060f869764a94b5b67cd0718
Rajvi Mehta <rajvi.m7@gmail.com>
2023-06-08 15:22:07 -0700

    Determine SKU (#18)
    * sku type
    
    * Minor changes
    
    * minor changes
    
    * add exit code
    
    * add log path as arg
    
    * remove sudo
    
    * Invoke nhc in script
    
    * add log path as optional arg
    
    * ndv5 sku
    
    * ignore case
    
    * remove exit code
    
    * Read me update
    
    * add support for relative file paths
    
    * PR comments
    
    * Minor change
    
    * Rename conf files
    
    ---------
    
    Co-authored-by: Ubuntu <azureuser@rajvi-nd96asrv4.vfmz02qryxyundbl3q24ozwj4g.ax.internal.cloudapp.net>
    Co-authored-by: Ubuntu <azureuser@rajvi-nd96amsr-v4.vfmz02qryxyundbl3q24ozwj4g.ax.internal.cloudapp.net>
    Co-authored-by: Ubuntu <azhpcuser@iad210859000070.j32po0hdf5wujnwyxy3ug3uv4f.bx.internal.cloudapp.net>
    Co-authored-by: almalinux Cloud User <azhpcuser@hx17666c200004E.aeid2alhjcluxleorasxbfdxtb.bx.internal.cloudapp.net>
    Co-authored-by: Ubuntu <azhpcuser@iad21d1ca000029.uoymqg2vopruth505qffmafr1e.bx.internal.cloudapp.net>

---
+++ run-health-checks.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+SKU=$( curl -H Metadata:true --max-time 10 -s "http://169.254.169.254/metadata/instance/compute/vmSize?api-version=2021-01-01&format=text")
+echo "Running health checks for $SKU SKU..."
+
+SKU="${SKU,,}"
+if echo "$SKU" | grep -q "nd96asr_v4"; then
+    conf_name="nd96asr_v4"
+elif echo "$SKU" | grep -q "nd96amsr_a100_v4"; then
+    conf_name="nd96amsr_a100_v4"
+elif echo "$SKU" | grep -q "nd96isr_h100_v5"; then
+    conf_name="nd96isr_h100_v5"
+elif echo "$SKU" | grep -q "hb120rs_v2"; then
+    conf_name="hb120rs_v2"
+elif echo "$SKU" | grep -q "hb120rs_v3"; then
+    conf_name="hb120rs_v3"
+elif echo "$SKU" | grep -q "hb176rs_v4"; then
+    conf_name="hb176rs_v4"
+elif echo "$SKU" | grep -q "hx176rs"; then
+    conf_name="hx176rs"
+else
+    echo "SKU health check currently not implemented"
+    exit 1
+fi
+
+log_path="${1:-./health.log}"
+log_path=$(realpath "$log_path")
+
+nhc CONFFILE=./conf/$conf_name.conf LOGFILE=$log_path TIMEOUT=300
