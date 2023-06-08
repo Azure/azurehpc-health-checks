04bbe6952675c7de301a7f0093a568dc4b39ba50
Ubuntu <azureuser@rajvi-nd96amsr-v4.vfmz02qryxyundbl3q24ozwj4g.ax.internal.cloudapp.net>
2023-06-08 14:59:23 +0000

    PR comments

--- run-health-checks.sh
+++ run-health-checks.sh
@@ -3,22 +3,23 @@
 SKU=$( curl -H Metadata:true --max-time 10 -s "http://169.254.169.254/metadata/instance/compute/vmSize?api-version=2021-01-01&format=text")
 echo "Running health checks for $SKU SKU..."
 
-if [ "${SKU,,}" = "standard_nd96asr_v4" ]; then
+SKU="${SKU,,}"
+if echo "$SKU" | grep -q "nd96asr_v4"; then
     conf_name="nd96asr_v4"
-elif [ "${SKU,,}" = "standard_nd96amsr_a100_v4" ]; then
+elif echo "$SKU" | grep -q "nd96amsr_a100_v4"; then
     conf_name="nd96amsr_v4"
-elif [ "${SKU,,}" = "standard_nd96isr_h100_v5" ]; then
+elif echo "$SKU" | grep -q "nd96isr_h100_v5"; then
     conf_name="nd96isr_v5"
-elif [ "${SKU,,}" = "standard_hb120rs_v2" ]; then
+elif echo "$SKU" | grep -q "hb120rs_v2"; then
     conf_name="hb120rs_v2"
-elif [ "${SKU,,}" = "standard_hb120rs_v3" ]; then
+elif echo "$SKU" | grep -q "hb120rs_v3"; then
     conf_name="hb120rs_v3"
-elif [ "${SKU,,}" = "standard_hbv4_176" ]; then
+elif echo "$SKU" | grep -q "hb176rs_v4"; then
     conf_name="hbv4_176"
-elif [ "${SKU,,}" = "standard_hx176rs" ]; then
+elif echo "$SKU" | grep -q "hx176rs"; then
     conf_name="hx176rs_4"
 else
-    echo "SKU: $SKU check not currently implemented"
+    echo "SKU health check not currently implemented"
     exit 1
 fi
 
