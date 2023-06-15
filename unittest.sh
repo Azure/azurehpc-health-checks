e25356134777f8f74f200705fc98cd1a535237f5
Ubuntu <azureuser@rajvi-nd96amsr-v4.vfmz02qryxyundbl3q24ozwj4g.ax.internal.cloudapp.net>
2023-06-15 19:19:28 +0000

    test BATS on github pipeline

---
+++ unittest.sh
@@ -0,0 +1,13 @@
+#!usr/bin/env bats
+@test "Test 1"
+{
+    run bash -c "echo 'Hello World'"
+    [ "$status" -eq 0 ]
+    [ "$output" = "Hello World" ]
+}
+
+@test "addition using bc"
+{
+  result="$(echo 2 + 2 | bc)"
+  [ "$result" -eq 4 ]
+}
