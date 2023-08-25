d46b65f99bb7aa885427f3e54b327715c399f688
rafsalas19 <70273488+rafsalas19@users.noreply.github.com>
2023-08-25 18:26:47 -0400

    Update run-health-checks.sh

--- run-health-checks.sh
+++ run-health-checks.sh
@@ -51,4 +51,4 @@ if [ $? -eq 0 ] && ! grep -q 'check_hw_ib 40 mlx5_an0:1' "$acc_file"; then
     echo -e "\n\n### Accelerate network check\n * || check_hw_ib 40 mlx5_an0:1\n * || check_hw_eth eth1">> $acc_file
 fi
 
-nhc CONFFILE=$(dirname "${BASH_SOURCE[0]}")/conf/$conf_name.conf LOGFILE=$log_path TIMEOUT=500
+nhc CONFFILE=$NHC_DIR/conf/$conf_name.conf LOGFILE=$log_path TIMEOUT=500
