2b80e87e9580632c0108c621ce6ffb23aa0c9498
Ubuntu <azhpcuser@ams25-3.lttkwgorh4fepfbv3wpixkbu0a.ax.internal.cloudapp.net>
2023-09-12 14:34:29 +0000

    rearanged commands

--- run-health-checks.sh
+++ run-health-checks.sh
@@ -45,10 +45,10 @@ fi
 
 #add accelerated network if applicable
 NHC_DIR=$(dirname "${BASH_SOURCE[0]}")
-acc_net=$(ibstatus mlx5_an0)
 acc_file="$NHC_DIR/conf/$conf_name.conf"
+acc_net=$(ibstatus mlx5_an0)
 if [ $? -eq 0 ] && ! grep -q 'check_hw_ib 40 mlx5_an0:1' "$acc_file"; then
-    echo -e "\n\n### Accelerate network check\n * || check_hw_ib 40 mlx5_an0:1\n * || check_hw_eth eth1">> $acc_file
+	echo -e "\n\n### Accelerate network check\n * || check_hw_ib 40 mlx5_an0:1\n * || check_hw_eth eth1">> $acc_file
 fi
 
-nhc CONFFILE=$NHC_DIR/conf/$conf_name.conf LOGFILE=$log_path TIMEOUT=500
+#nhc CONFFILE=$NHC_DIR/conf/$conf_name.conf LOGFILE=$log_path TIMEOUT=500
