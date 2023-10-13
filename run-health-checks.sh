c6742502016d71118db5c962b6865e115378f419
rafsalas19 <70273488+rafsalas19@users.noreply.github.com>
2023-10-13 15:32:55 -0400

    IB write Loop, test files, and Doc update (#38)
    * Ndv4, replacing ib_write test w/ loop_gdr test. NDv5 replacing NCCL and IBwrite IB test with loop GDR
    
    * added small fix to link flap check
    
    * adjust expected values and add documentation for tests
    
    * adding test conf files for expected failure unit test
    
    * renaming of the IB write tests
    
    * fixing AN issue
    
    * bug fixes
    
    * an fix
    
    ---------
    
    Co-authored-by: Ubuntu <azhpcuser@ndv4d4c2f000002.1522po4gonsuzpxliyxrsh25ud.jx.internal.cloudapp.net>
    Co-authored-by: Ubuntu <rafsalas@HBtestVM.cjhdhogqmwxenkt1ulhotgyz3h.bx.internal.cloudapp.net>

--- run-health-checks.sh
+++ run-health-checks.sh
@@ -74,33 +74,46 @@ if [ -z "$CUSTOM_CONF" ]; then
     SKU="${SKU,,}"
     if echo "$SKU" | grep -q "nd96asr_v4"; then
         conf_name="nd96asr_v4"
+	an_rate=40
     elif echo "$SKU" | grep -q "nd96amsr_a100_v4"; then
         conf_name="nd96amsr_a100_v4"
+	an_rate=40
     elif echo "$SKU" | grep -q "nd96isr_h100_v5"; then
         conf_name="nd96isr_h100_v5"
+	an_rate=100
     elif echo "$SKU" | grep -q "hb120rs_v2"; then
         conf_name="hb120rs_v2"
     elif echo "$SKU" | grep -q "hb120rs_v3"; then
         conf_name="hb120rs_v3"
     elif echo "$SKU" | grep -q "hb176rs_v4"; then
         conf_name="hb176rs_v4"
+	an_rate=100
     elif  echo "$SKU" | grep -q "hb176-144rs_v4"; then
         conf_name="hb176-144rs_v4"
+	an_rate=100
     elif  echo "$SKU" | grep -q "hb176-96rs_v4"; then
         conf_name="hb176-96rs_v4"
+	an_rate=100
     elif  echo "$SKU" | grep -q "hb176-48rs_v4"; then
         conf_name="hb176-48rs_v4"
+	an_rate=100
     elif  echo "$SKU" | grep -q "hb176-24rs_v4"; then
         conf_name="hb176-24rs_v4"
+	an_rate=100
     elif echo "$SKU" | grep -q "hx176rs"; then
         conf_name="hx176rs"
+	an_rate=100
     elif  echo "$SKU" | grep -q "hx176-144rs"; then
         conf_name="hx176-144rs"
+	an_rate=100
     elif  echo "$SKU" | grep -q "hx176-96rs"; then
         conf_name="hx176-96rs"
+	an_rate=100
     elif  echo "$SKU" | grep -q "hx176-48rs"; then
+	an_rate=100
         conf_name="hx176-48rs"
     elif  echo "$SKU" | grep -q "hx176-24rs"; then
+	an_rate=100
         conf_name="hx176-24rs"
     else
         echo "The vm SKU '$SKU' is currently not supported by Azure health checks." | tee -a $OUTPUT_PATH
@@ -112,8 +125,8 @@ if [ -z "$CUSTOM_CONF" ]; then
     #add accelerated network if applicable, when using the default conf files, skip if an explicit conf file is specified
     acc_file=$CONF_FILE
     acc_net=$(ibstatus mlx5_an0)
-    if [ $? -eq 0 ] && ! grep -q 'check_hw_ib 40 mlx5_an0:1' "$acc_file"; then
-        echo -e "\n\n### Accelerate network check\n * || check_hw_ib 40 mlx5_an0:1\n * || check_hw_eth eth1" >> $acc_file
+    if [ $? -eq 0 ] && [ -n "$an_rate" ] && ! grep -q 'mlx5_an0:1' "$acc_file"; then
+        echo -e "\n\n### Accelerate network check\n * || check_hw_ib $an_rate  mlx5_an0:1\n * || check_hw_eth eth1" >> $acc_file
     fi
 
 fi
