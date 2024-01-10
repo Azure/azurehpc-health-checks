86575eb24766cadfb0f298794d09fd30b6c1d9fc
Ubuntu <rafsalas@ndv4.ydru00tc21wuzf2z1e23k3cpig.jx.internal.cloudapp.net>
2024-01-10 18:37:06 +0000

    address pr comments

--- test/bats-unit-test/run-bats-Unit-tests.sh
+++ test/bats-unit-test/run-bats-Unit-tests.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-sudo apt-get install -y bats &> /dev/null
-sudo yum install -y bats &> /dev/null
-
+if command -v bats &> /dev/null; then
+    echo "Bats not found. Attempting to install..."
+    distro=`awk -F= '/^NAME/{print $2}' /etc/os-release`
+    if [[ $distro =~ "Ubuntu" ]]; then
+        sudo apt-get install -y bats
+    else
+        sudo yum install -y bats
+    fi
+fi
 
 source $(dirname "${BASH_SOURCE[0]}")/../../aznhc_env_init.sh
 echo $AZ_NHC_ROOT
 $AZ_NHC_ROOT/test/bats-unit-test/bats-unit-tests.sh
+
+exit 0
