e7f315b242bfa43d77ead51dc124b6beac74f13d
Ubuntu <rafsalas@nhc-A100vm.dav1onuyw5fejl0wtzkr0deqkf.bx.internal.cloudapp.net>
2024-02-09 20:14:40 +0000

    modify/fix docker pull script

--- dockerfile/pull-image-acr.sh
+++ dockerfile/pull-image-acr.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-DOCK_IMG_NAME="aznhc.azurecr.io/nvrt"
+DOCK_IMG_NAME="aznhc.azurecr.io/nvidia-rt"
 REPO_USERNAME="az-nhc-tok"
 vaultName="aznhc-kv"
 secretName="az-nhc-tok"
 resourceGroupName="azure-nhc-resources"
-managedIdentityName="az-nhc-id"
+managedIdentityID="335a45e3-ba51-4c7a-b3f7-2e109f488cee"
 subscriptionId="75c5e023-db83-4675-8531-fd0150c82176"
 
-az login --identity -u "$managedIdentityName"
+az login --identity -u "$managedIdentityID"
 az account set --subscription "$subscriptionId"
 
 secretValue=$(az keyvault secret show --vault-name "$vaultName" --name "$secretName" --query "value" --output tsv)
-
+echo $secretValue
 sudo docker login aznhc.azurecr.io --password $secretValue --username $REPO_USERNAME 
 
 sudo docker pull $DOCK_IMG_NAME
