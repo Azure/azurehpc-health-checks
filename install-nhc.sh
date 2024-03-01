2303ff3f9db199ac2de8da8419593b2b7890517d
Rajvi Mehta <rameht@microsoft.com>
2024-03-01 15:48:43 -0500

    Remove locate installation and references

--- install-nhc.sh
+++ install-nhc.sh
@@ -70,20 +70,18 @@ distro_check=$( cat /etc/os-release | grep -i ID_LIKE=)
 distro=`awk -F= '/^NAME/{print $2}' /etc/os-release`
 
 if [[ $distro_check =~ "debian" ]]; then
-  sudo DEBIAN_FRONTEND=noninteractive apt-get install -y libpci-dev hwloc build-essential libboost-program-options-dev libssl-dev plocate cmake
+  sudo DEBIAN_FRONTEND=noninteractive apt-get install -y libpci-dev hwloc build-essential libboost-program-options-dev libssl-dev cmake
 elif [[ $distro =~ "AlmaLinux" ]]; then
-  sudo dnf install -y pciutils-devel hwloc openssl-devel boost-devel mlocate cmake
+  sudo dnf install -y pciutils-devel hwloc openssl-devel boost-devel cmake
 # this will attempt to install for the following os's: CentOS, RHEL, Fedora
 elif [[ $distro_check =~ "CentOS" ]] || [[ $distro_check =~ "Rhel" ]] || [[ $distro_check =~ "fedora" ]]; then
-  sudo yum install -y  pciutils-devel hwloc openssl-devel boost-devel mlocate cmake > /dev/null
+  sudo yum install -y  pciutils-devel hwloc openssl-devel boost-devel cmake > /dev/null
   echo "$distro version is not officially supported, proceed w/ caution."
 else
   echo "OS version $distro is not supported. Proceed w/ caution."
 fi
 
 # Install build tools
-# Install locate
-sudo apt-get install mlocate
 # Check cmake version + install if necessary
 output=$(cmake --version | sed -n 1p | sed 's/[^0-9]*//g')
 export NHC_CMAKE=cmake
