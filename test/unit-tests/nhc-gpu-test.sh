7b310b8f4a2707352a7b22c53d2368d4eb2dab97
Elizabeth Fatade <61216259+ElizabethFatade@users.noreply.github.com>
2024-03-26 14:01:04 -0700

    Efatade/refactor exit msg (#99)
    * Refactor di message to include FaultCodes
    
    * Updated Fault Codes
    
    * Updated Fault Codes
    
    ---------
    
    Co-authored-by: Ubuntu <efatade@efatade-ndv2.nq3vxuazwo1unk43txafup2zqc.bx.internal.cloudapp.net>

--- test/unit-tests/nhc-gpu-test.sh
+++ test/unit-tests/nhc-gpu-test.sh
@@ -136,11 +136,17 @@ ppending_blacklist_sections=($(awk '/Pending Page Blacklist / {print $NF}' $AZ_N
     set +e
     flag="false"
     error_msg=''
+    re='^[0-9]+$'
+
     for i in "${!gpu_sections[@]}"; do
         # Extract SBE and DBE values
         gpu=${gpu_sections[i]}
         sbe=${sbe_sections[i]}
         dbe=${dbe_sections[i]}
+
+        if ! [[ $sbe =~ $re ]] || ! [[ $dbe =~ $re ]]; then
+            continue
+        fi
 
         # Calculate the sum of SBE and DBE pages
         total=$((sbe + dbe))
